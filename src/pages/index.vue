f95c9625a9db649fac5bcf4b7ef4fe23a1f3805d
mirumi <info@milmemo.net>
2022-06-14 15:05:48 +0900

    change: to Nuxt3 and create scaffold for template

---
+++ src/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Index page</h1>
+  <NuxtLink to="/page-sample">Home page</NuxtLink>
+</template>
+
+<script setup lang="ts">
+
+definePageMeta({
+  title: "My home page",
+  // middleware: ["navigation-guards"],
+})
+
+
+// // router.push()は使えないの？
+// function navigate(){
+//   return navigateTo({
+//     path: "/search",
+//     query: {
+//       name: name.value,
+//       type: type.value
+//     }
+//   })
+// }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
